7f1607e67f639fb4a47774cb49b05d3dcbdd1832
Kaloyan Mitev <Kaloyan.Mitev.extern@collenda.com>
2023-01-16 14:52:26 +0200

    Add security

--- scrapy_demo/pipelines.py
+++ scrapy_demo/pipelines.py
@@ -7,6 +7,34 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import requests
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "prod/OditoAPI/Admin"
+    region_name = "eu-central-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    return get_secret_value_response['SecretString']
+
+    # Your code goes here.
 
 
 class ScrapyDemoPipeline:
@@ -22,7 +50,7 @@ class EndpointPipeline:
             'collection': article['collection'],
             'datetime_posted':article['datetime_posted']
         }
-        headers = {'Content-Type': 'application/json'}
+        headers = {'Content-Type': 'application/json', 'x-api-key': get_secret()}
         r = requests.post('https://api.metaquants.xyz/v1/scraping', json=data, headers=headers)
         print(r.text)
         return article
