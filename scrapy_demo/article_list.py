08d1ae19c47f98a6ab08ea793980a3b909a8f04f
KaloyanMitev <dm.kaloyan@gmail.com>
2023-04-03 23:13:23 +0300

    Update supported collections

--- scrapy_demo/article_list.py
+++ scrapy_demo/article_list.py
@@ -1,1 +1,1 @@
-list = ['bored ape yacht club','doodles','azuki','moonbirds']
+list = ['azuki','cryptopunks','bored ape yacht club','bored ape kennel club','mutant ape yacht club','pudgy penguins','moonbirds','clone x','veefriends','meebits','doodles']
