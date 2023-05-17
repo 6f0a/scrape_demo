9aed076edd8595f60ed34c7e08ccfc8da4093f17
KaloyanMitev <dm.kaloyan@gmail.com>
2023-05-17 17:51:09 +0300

    Update article_list.py
    Add new 11 collections

--- scrapy_demo/article_list.py
+++ scrapy_demo/article_list.py
@@ -1,1 +1,2 @@
-list = ['azuki','cryptopunks','bored ape yacht club','bored ape kennel club','mutant ape yacht club','pudgy penguins','moonbirds','clone x','veefriends','meebits','doodles']
+list = ['azuki','cryptopunks','bored ape yacht club','bored ape kennel club','mutant ape yacht club','pudgy penguins','moonbirds','clone x','veefriends','meebits','doodles', 
+        'nakamigos','milady maker','degods','the potatoz','killabears','world of women','mfers','dickbuts','the captainz','onchain monkey','rektguy']
